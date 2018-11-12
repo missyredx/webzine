0abdd1c35d9907bf2297e98200a6021e52f0548b
missyredx <43077518+missyredx@users.noreply.github.com>
2018-11-12 11:56:40 -0500

    Add files via upload

---
+++ js/zine.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="title" content="Art=Work">
+        <meta name="Description" content="A project website designed by Jenssy Morales for MMP 200.">
+    
+	<meta name="Keywords" content="scripting,creativity,typography,photography,advertising,interactive,web,web design">
+    
+	<title>Jenssy Morales, Web Design for MMP</title>
+    
+    <link rel="stylesheet" href="css/css.css" type="text/css">
+    
+</head>
+    
+    
+<body>java</body>
+
+</html>
